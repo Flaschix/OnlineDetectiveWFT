1b1ede79b4da466b83f44d09179f7d05fd600a15
Flaschix <mishasigalov1@gmail.com>
2024-08-27 14:27:22 +0300

    off debug

--- public/game.js
+++ public/game.js
@@ -23,7 +23,7 @@ const config = {
     physics: {
         default: 'matter',
         matter: {
-            debug: true,
+            // debug: true,
             gravity: { y: 0 }
         }
     },
